5fa40100e5f5f02a1dbbaf0fb77360992c2a322f
AizatKarimzhanova <aizatkarimova00@gmail.com>
2025-12-09 17:35:41 +0600

    изменить

--- lesson_1.py
+++ lesson_1.py
@@ -1,6 +1,9 @@
 
 class Car:
-    
+    #инициализатор(конструктор)
+    """def __init__(self):       const
+        self.color = "red"
+        self.model = "Matiz"""
     def __init__(self, color, model):
         self.color = color
         self.model = model
@@ -8,29 +11,22 @@ class Car:
 
     def drive_to(self, destination):
         print(f"Car {self.model} driving to {destination}")
-
+        
     def change_color(self, new_color):
         self.color = new_color
         print(f"Car {self.model} changed to {self.color}")
 
-class Bus(Car):
-    def __init__(self, color, model, number):
-        super().__init__(color, model)
-        self.number = number
+car_honda = Car(color = "black", model = "Honda")
+car_subaru = Car("blue", "Subaru")
+print(car_honda)
+print(car_honda.color, car_honda.model, car_honda.fined)
+car_honda.color = "green"
+print(car_honda.color, car_honda.model, car_honda.fined)
+print(car_subaru)
+print(car_subaru.color, car_subaru.model)
+car_honda.year = 2020
+print(car_honda.year)
 
-class Truct(Car):
-    def drive_to(self, destination):
-        print(f"Truct {self.model} driving to {destination}")
-
-    def test(self):
-        print(f"truct ")
-
-
-
-truct_1 = Truct("blac", "Mers")
-truct_1.drive_to("Bishkek")
-truct_1.test()
-
-bus_1 = Bus("blu", "Honda",6)
-print(bus_1.number)
-
+car_subaru.drive_to("Bishkek")
+car_honda.change_color("yellow")
+car_subaru.change_color("skyblue")
